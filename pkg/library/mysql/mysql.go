18c54abacac1a2542150c875e2be6d7c12033d9c
henprasetya@yahoo.co.id <henprasetya@yahoo.co.id>
2021-10-21 13:41:49 +0700

    remove user

--- pkg/library/mysql/mysql.go
+++ pkg/library/mysql/mysql.go
@@ -2,7 +2,9 @@ package mysql
 
 import (
 	"database/sql"
+
 	"github.com/jinzhu/gorm"
+
 	//_ "github.com/jinzhu/gorm/dialects/postgres"
 	"log"
 
@@ -29,7 +31,7 @@ func dbConnection() (*MySql, error) {
 	// dbUri := os.Getenv("db_uri")
 	// dbPass := os.Getenv("db_pass")
 	// dbUser := os.Getenv("db_user")
-	dsn := "mti:UX86E3vyfvo46trh@tcp(139.180.217.246:3306)/kopbidb?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "xxx:xxxx@tcp(xxx:3306)/kopbidb?charset=utf8mb4&parseTime=True&loc=Local"
 	log.Print(dsn)
 	db, err = gorm.Open("mysql", dsn)
 	if err != nil {
